5948568fbd021d57d2a5b30300494ab6cc33ec79
masprog2022 <masprog2022@gmail.com>
2023-08-11 13:55:40 +0100

    :sparkles: feat: implementado interface - CreditService(findByCreditCode)

--- src/main/kotlin/com/masprogtechs/credit/application/system/repositories/CreditRepository.kt
+++ src/main/kotlin/com/masprogtechs/credit/application/system/repositories/CreditRepository.kt
@@ -3,6 +3,10 @@ package com.masprogtechs.credit.application.system.repositories
 import com.masprogtechs.credit.application.system.entities.Credit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
-interface CreditRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long>{
+    fun findByCreditCode(creditCode: UUID): Credit?
+}
+
