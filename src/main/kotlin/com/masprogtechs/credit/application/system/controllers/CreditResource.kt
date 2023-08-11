49fbca54130bd0a9a1ab77e444b9fe36a2f46c9b
masprog2022 <masprog2022@gmail.com>
2023-08-11 18:17:14 +0100

    :sparkles: feat: endpoint CreditResource - FindAllCreditCode

--- src/main/kotlin/com/masprogtechs/credit/application/system/controllers/CreditResource.kt
+++ src/main/kotlin/com/masprogtechs/credit/application/system/controllers/CreditResource.kt
@@ -1,10 +1,12 @@
 package com.masprogtechs.credit.application.system.controllers
 
 import com.masprogtechs.credit.application.system.dto.CreditDto
+import com.masprogtechs.credit.application.system.dto.CreditView
 import com.masprogtechs.credit.application.system.dto.CreditViewList
 import com.masprogtechs.credit.application.system.entities.Credit
 import com.masprogtechs.credit.application.system.services.impl.CreditService
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import java.util.stream.Collectors
 
 @RestController
@@ -23,5 +25,11 @@ class CreditResource(
     fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long) : List<CreditViewList>{
        return this.creditService.findAllByCustomer(customerId).stream()
            .map { credit: Credit -> CreditViewList(credit) }.collect(Collectors.toList())
+    }
+
+    @GetMapping
+    fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long, @PathVariable creditCode: UUID): CreditView {
+       val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
     }
 }
